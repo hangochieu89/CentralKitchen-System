5694f8e1ca2af454c1a2b9e6e30311dd60ff66b7
WilliamCodyz <ddinhphat9@gmail.com>
2026-04-28 16:32:40 +0700

    feat: Add data models from the Service to the controller.

--- src/main/java/com/centralkitchen/backend/controller/KitchenStaffController.java
+++ src/main/java/com/centralkitchen/backend/controller/KitchenStaffController.java
@@ -1,6 +1,9 @@
 package com.centralkitchen.backend.controller;
 
+import com.centralkitchen.backend.service.KitchenStaffService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/kitchen-staff")
 public class KitchenStaffController {
 
+    @Autowired
+    private KitchenStaffService kitchenStaffService;
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("orders", kitchenStaffService.getAllOrders());
+        model.addAttribute("plans", kitchenStaffService.getAllProductionPlans());
+        model.addAttribute("batches", kitchenStaffService.getAllProductBatches());
+        model.addAttribute("inventories", kitchenStaffService.getAllInventories());
+        model.addAttribute("receipts", kitchenStaffService.getAllGoodsReceipts());
+        model.addAttribute("receiptDetails", kitchenStaffService.getAllGoodsReceiptDetails());
+        
         return "forward:/kitchen-staff/index.jsp";
     }
 }
